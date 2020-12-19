4ca451e33ca595921c05ddc3e6bd2a7d979024a1
Adam <adamw43081@gmail.com>
2020-12-19 12:40:04 -0800

    Fixed spacing and other small issues.

--- src/Main.java
+++ src/Main.java
@@ -4,40 +4,50 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		printTree(6,6,1);
+		int numRows = 7;
+		for(int i = 0; i <=2; i++) {
+			System.out.println();
+		}
+		if(numRows <3) {
+			System.out.println("Small Tree!");
+			for(int i = 0; i <=2; i++) {
+				System.out.println();
+			}
+		}
+		printTree(numRows, numRows,1);
 	}
 	private static void printTree(int startRow, int currentRow, int numSpace) {
 		if (currentRow <= 1) {
 			int max;
 			if(startRow <= 5) {
 				max = 4;
-				for (int i = 0; i<6; i++) {
-					System.out.println();
-				}
 			}else {
 				max = 4*(startRow-1) + 1;
 			}
-			printFloor(max/2);
+			printPadding(numSpace/2 +2 , "_");
 			if(currentRow ==1) {
 				System.out.print("|");
 			}
-			printFloor(max/2);
+			printPadding(numSpace/2 + 2, "_");
 		}else {
 			int numGroups = (int) Math.pow(2, currentRow-2);
+			printPadding((numSpace)/2+2, " ");
 			for(int i = 0; i<numGroups; i++) {
 				System.out.print("|");
-				printFloor(numSpace);
+				printPadding(numSpace, "_");
 				System.out.print("|");
+				printPadding(numSpace, " ");
 			}
 			System.out.println();
 			printTree(startRow, currentRow-1, numSpace*2 +1);
 		}
 	}
 	
-	private static void printFloor(int num) {
+	private static void printPadding(int num, String s) {
 		for(int i = 0; i<num; i++) {
-			System.out.print("_");
+			System.out.print(s);
 		}
 	}
+	
 
 }
