02a68bd2127b9cd16d54488cf594bcc36659492a
Adam <adamw43081@gmail.com>
2021-03-22 21:47:12 -0700

    Added drawable option for Python.

--- BinaryTree.py
+++ BinaryTree.py
@@ -2,11 +2,16 @@ import pygame, sys
 from pygame.locals import *
 from tkinter import * 
 from math import *
+from time import sleep
+from pygame.event import wait
+from numpy.core.defchararray import lower
 
 try:
     print("\n")
     h = int(input("height of tree: "))
     split_angle = int(input("angle between branches (degrees): "))*pi/360
+    draw = input("Do you want to see it be drawn? (y/n): ")
+    drawbool = (lower(draw) == "y")
 except ValueError:
     print("Oops! One of your inputs was an invalid number. Please restart the program and try again.")
     pygame.quit()
@@ -49,24 +54,33 @@ def drawTreeTrunk(h):
         drawTreeLeaves(h-1,length-length_scalar,0,x1,y1-length);
 
 def drawTreeLeaves(h, length, start_angle,x1,y1):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
     if (h >= 1):
         p1 = (x1,y1)
         p2 = (x1 + length*sin(start_angle + split_angle), y1 - length*cos(start_angle + split_angle));
         p3 = (x1 + length*sin(start_angle - split_angle), y1 - length*cos(start_angle - split_angle));
         pygame.draw.line(display_surf,WHITE,p1,p2,4)
         pygame.draw.line(display_surf,WHITE,p1,p3,4)
+        if(drawbool):
+            pygame.display.flip()
+            wait(5)
         drawTreeLeaves(h-1,length-length_scalar, start_angle + split_angle, x1 + length*sin(start_angle + split_angle), y1 - length*cos(start_angle + split_angle))
         drawTreeLeaves(h-1,length-length_scalar, start_angle - split_angle, x1 + length*sin(start_angle - split_angle), y1 - length*cos(start_angle - split_angle))
 
 
 #Main Loop
-while True:
+while True: 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()  
+            sys.exit()
     drawArena();
     drawTreeTrunk(h);
     pygame.display.flip()
+    wait(1000)
+
 if __name__=='__main__':
     main()
